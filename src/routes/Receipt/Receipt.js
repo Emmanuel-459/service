29374127823144ab3bacb03f1e645c52d1fca3e2
Oscar Alba <oscaralba33@gmail.com>
2023-06-19 18:37:20 -0400

    Add useState Hook for Receipts Number

--- src/routes/Receipt/Receipt.js
+++ src/routes/Receipt/Receipt.js
@@ -1,7 +1,18 @@
-import { React } from "react";
+import React  from "react";
 import "./Receipt.css";
 
 function Receipt() {
+
+  let today = new Date();
+  let dd = String(today.getDate()).padStart(2, "0");
+  let mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+  let yyyy = today.getFullYear();
+
+  today = mm + "/" + dd + "/" + yyyy;
+
+
+  const [num, setNum] = React.useState(60);
+
   return (
     <div className="containerPrincipalReceipt">
       <div className="header">
@@ -9,9 +20,9 @@ function Receipt() {
           <h4>ENTRETIEN MÉNAGER</h4>
           <h4>EDNA TRUJILLO</h4>
         </div>
-        <div>
+        <div className="padding-right">
           <p>Receipts</p>
-          <p>No. 0000002</p>
+          <p>No. 0000{num}</p>
         </div>
       </div>
       <div className="blue-header">
@@ -28,13 +39,21 @@ function Receipt() {
       <div className="content">
         <div className="flex">
           <p>MONTANT DÉCAISSÉ:</p>
-          <input type="number" name="dollar" className="width-40px margin-left-2" /> 
+          <input
+            type="number"
+            name="dollar"
+            className="width-40px margin-left-2"
+          />
           <p className="margin-left-2">CAD</p>
         </div>
         <hr />
         <div className="flex">
           <p>EXPÉDIÉ À:</p>
-          <input type="text" name="expide" className="width-180px margin-left-2" />
+          <input
+            type="text"
+            name="expide"
+            className="width-180px margin-left-2"
+          />
         </div>
         <hr />
         <div className="flex">
@@ -47,15 +66,12 @@ function Receipt() {
           </select>
         </div>
       </div>
+      <button
+        onClick={() => { setNum(num+1);}}>
+        Click Me!!
+      </button>
     </div>
   );
 }
-
-let today = new Date();
-let dd = String(today.getDate()).padStart(2, "0");
-let mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
-let yyyy = today.getFullYear();
-
-today = mm + "/" + dd + "/" + yyyy;
 
 export { Receipt };
